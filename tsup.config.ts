2af12a36327f169bdb6a895aee9de16316d47248
omaroskars <omar@konstant.is>
2025-01-22 12:58:36 +0100

    fixups

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig(() => ({
-  entry: ['src/index.ts', 'src/canUseDOM.ts'], // Include all necessary entry points
-  outDir: 'dist', // Output directory
-  format: ['cjs', 'esm'], // Build both CommonJS and ESM
-  sourcemap: true, // Generate sourcemaps
-  dts: true, // Generate declaration files
   clean: true, // Clean the output directory before building
+  dts: true, // Generate declaration files
+  entry: ['src/index.ts', 'src/canUseDOM.ts'], // Include all necessary entry points
   external: ['react'], // Mark React as external
+  format: ['cjs', 'esm'], // Build both CommonJS and ESM
+  outDir: 'dist', // Output directory
+  sourcemap: true, // Generate sourcemaps
 }))
