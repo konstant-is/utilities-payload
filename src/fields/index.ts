ed77ada5147fa0f0d47a0d85e8d5c982b06e6cf4
omaroskars <omar@konstant.is>
2025-01-22 13:30:36 +0100

    export fields

---
+++ src/fields/index.ts
@@ -0,0 +1,1 @@
+export * from './arrayRowLabelField.js'
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig(() => ({
   clean: true, // Clean the output directory before building
   dts: true, // Generate declaration files
-  entry: ['src/index.ts', 'src/exports/client.ts'], // Include all necessary entry points
+  entry: ['src/index.ts', 'src/fields/index.ts', 'src/exports/client.ts'], // Include all necessary entry points
   external: ['react', 'payload', '@payloadcms/ui'], // Mark React as external
 
   format: ['cjs', 'esm'], // Build both CommonJS and ESM
