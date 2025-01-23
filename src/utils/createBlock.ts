4b5846d0ad69c544ca1a30e58b78349125dd0df5
omaroskars <omar@konstant.is>
2025-01-23 10:36:45 +0100

    add create block with settings

--- src/utils/createBlock.ts
+++ src/utils/createBlock.ts
@@ -1,5 +1,7 @@
 import type { Block } from 'payload'
 
+import { field } from './field.js'
+
 export const createBlock = (block: Block): Block => {
   const fallbackInterfaceName = () =>
     block.slug.includes('Block') ? block.slug : `${block.slug}Block`
@@ -7,5 +9,48 @@ export const createBlock = (block: Block): Block => {
   return {
     ...block,
     interfaceName: block?.interfaceName || fallbackInterfaceName(),
+  }
+}
+type BlockWithSettingsProps<P = unknown> = P
+type BlockWithSettingsFn = <P>(props: BlockWithSettingsProps<P>) => Block
+export const createBlockWithSettings = (fn: BlockWithSettingsFn) => {
+  return (props: BlockWithSettingsProps) => {
+    const result = fn(props)
+
+    return createBlock({
+      ...result,
+      fields: [
+        field({
+          type: 'tabs',
+          tabs: [
+            { fields: result.fields, label: 'Content' },
+            {
+              name: 'settings',
+              fields: [
+                field({
+                  name: 'className',
+                  type: 'text',
+                  admin: {
+                    description: 'Adds custom classes to the block',
+                  },
+                  hasMany: true,
+                  label: 'Class Name',
+                }),
+                field({
+                  name: 'id',
+                  type: 'text',
+                  admin: {
+                    description: 'Add custom ID to the block',
+                  },
+                  label: 'ID',
+                  required: false,
+                }),
+              ],
+              label: 'Settings',
+            },
+          ],
+        }),
+      ],
+    })
   }
 }
