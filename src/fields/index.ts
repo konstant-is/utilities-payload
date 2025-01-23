56ffeec18b0756030de7ebcb38ccd27c8dd7297f
omaroskars <omar@konstant.is>
2025-01-23 13:57:12 +0100

    add link field

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,5 +1,6 @@
 export * from './addressField.js'
 export * from './arrayRowLabelField.js'
+export * from './linkField.js'
 export * from './openingHoursField.js'
 export * from './socialsField.js'
 export * from './timeField.js'
